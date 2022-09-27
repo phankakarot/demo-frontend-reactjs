137aa359618b9bb729890e1ba8666f41fd469293
macbook <macbook@MacBook-Cua-Hieu.local>
2022-09-28 00:22:40 +0700

    add style

--- ConverToReact/react-app/src/App.js
+++ ConverToReact/react-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './page/Home/Home';
+import './assests/css/style-import.css'
+// import './assests/css/style-rtl.css'
 
 function App() {
   return (
