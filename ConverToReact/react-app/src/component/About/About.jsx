00f78c996fbb7e365318523870591932ccab976b
macbook <macbook@MacBook-Cua-Hieu.local>
2022-09-27 18:25:42 +0700

    refactor code on About component

--- ConverToReact/react-app/src/component/About/About.jsx
+++ ConverToReact/react-app/src/component/About/About.jsx
@@ -2,98 +2,58 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div id="section-about" class="section page-section m-0">
 
-					<div class="container clearfix">
+	const featureList = [{
+		link: '/',
+		icon: 'spa-sandals',
+		title: 'Shower Services',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas omnis nam molestias minus ipsa, placeat!'
+	}, {
+		link: '/',
+		icon: 'spa-sandals',
+		title: 'Shower Services',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas omnis nam molestias minus ipsa, placeat!'
+	}, {
+		link: '/',
+		icon: 'spa-sandals',
+		title: 'Shower Services',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas omnis nam molestias minus ipsa, placeat!'
+	}]
 
-						<div class="row">
-							<div class="col-lg-6">
-								<div class="heading-block border-bottom-0 bottommargin-sm">
-									<h3 class="cookie-font text-capitalize color">About Us</h3>
-								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, libero voluptas iusto quo illum quam possimus provident, deserunt est minima quisquam amet magnam animi? Enim adipisci facilis voluptas odit! Facilis rerum ipsam modi perspiciatis exercitationem explicabo, tempore soluta culpa cupiditate!</p>
-							</div>
-							<div class="col-lg-6">
-								<img class="bottommargin-lg mx-auto img-fluid" src="demos/spa/images/others/1.png" alt="Image"/>
-							</div>
+	return (
+		<div id="section-about" class="section page-section m-0">
+
+			<div class="container clearfix">
+
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="heading-block border-bottom-0 bottommargin-sm">
+							<h3 class="cookie-font text-capitalize color">About Us</h3>
 						</div>
-						<div class="row">
-							<div class="col-lg-4 bottommargin">
-								<div class="feature-box fbox-plain" data-animate="fadeIn">
-									<div class="fbox-icon">
-										<a href="/"><i class="spa-towel"></i></a>
-									</div>
-									<div class="fbox-content">
-										<h3>Shower Services</h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas omnis nam molestias minus ipsa, placeat!</p>
-									</div>
-								</div>
-							</div>
-
-							<div class="col-lg-4 bottommargin">
-								<div class="feature-box fbox-plain" data-animate="fadeIn" data-delay="200">
-									<div class="fbox-icon">
-										<a href="/"><i class="spa-sandals"></i></a>
-									</div>
-									<div class="fbox-content">
-										<h3>Foot Treatments</h3>
-										<p>Facere aliquam itaque quia recusandae, corporis fugit fugiat eaque, accusamus officiis reprehenderit.</p>
-									</div>
-								</div>
-							</div>
-
-							<div class="col-lg-4 bottommargin">
-								<div class="feature-box fbox-plain" data-animate="fadeIn" data-delay="800">
-									<div class="fbox-icon">
-										<a href="/"><i class="spa-cup-flower"></i></a>
-									</div>
-									<div class="fbox-content">
-										<h3>Internal Beautification</h3>
-										<p>Velit id facilis odit aliquid laudantium. Tempore, sequi. Harum nesciunt, magni aperiam est?</p>
-									</div>
-								</div>
-							</div>
-
-							<div class="w-100 clear"></div>
-
-							<div class="col-lg-4 bottommargin">
-								<div class="feature-box fbox-plain" data-animate="fadeIn" data-delay="1000">
-									<div class="fbox-icon">
-										<a href="/"><i class="spa-meditation"></i></a>
-									</div>
-									<div class="fbox-content">
-										<h3>Yoga &amp; Meditation</h3>
-										<p>Voluptatum distinctio laboriosam, excepturi delectus nam fugit obcaecati reiciendis neque iste!</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-4 bottommargin">
-								<div class="feature-box fbox-plain" data-animate="fadeIn" data-delay="1000">
-									<div class="fbox-icon">
-										<a href="/"><i class="spa-comb"></i></a>
-									</div>
-									<div class="fbox-content">
-										<h3>Hair Dressing</h3>
-										<p>Quis, tenetur. Nam explicabo modi pariatur, blanditiis distinctio sapiente fugiat accusantium.</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-4 bottommargin">
-								<div class="feature-box fbox-plain" data-animate="fadeIn" data-delay="1000">
-									<div class="fbox-icon">
-										<a href="/"><i class="spa-flowers-feet"></i></a>
-									</div>
-									<div class="fbox-content">
-										<h3>Herbal Pedicure</h3>
-										<p>Veniam atque corporis commodi. Quidem odit, necessitatibus quo tenetur! Harum similique illo eligendi!</p>
-									</div>
-								</div>
-							</div>
-						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, libero voluptas iusto quo illum quam possimus provident, deserunt est minima quisquam amet magnam animi? Enim adipisci facilis voluptas odit! Facilis rerum ipsam modi perspiciatis exercitationem explicabo, tempore soluta culpa cupiditate!</p>
+					</div>
+					<div class="col-lg-6">
+						<img class="bottommargin-lg mx-auto img-fluid" src="demos/spa/images/others/1.png" alt="Image" />
 					</div>
 				</div>
-  )
+				<div class="row">
+					{featureList?.map((item, index) => (
+						<div class="col-lg-4 bottommargin">
+							<div class="feature-box fbox-plain" data-animate="fadeIn">
+								<div class="fbox-icon">
+									<a href={item.link}><i class={item.icon}></i></a>
+								</div>
+								<div class="fbox-content">
+									<h3>{item.title}</h3>
+									<p>{item.desc}</p>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default About
